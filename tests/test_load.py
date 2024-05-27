e9ec1a4a77625172c900511c2d3d1b7b310376d6
SolidSalad <grimmieboy17@gmail.com>
2024-05-27 14:48:33 +0000

    added pre commit hooks

---
+++ tests/test_load.py
@@ -0,0 +1,13 @@
+from exercise.load import collect
+
+
+def test_generate_query_params():
+    actual = collect.generate_query_params("test", 1, 10)
+
+    expected = {
+        "$$app_token": "test",
+        "$where": "tpep_pickup_datetime between '2021-01-01T00:00:00' and '2021-02-01T00:00:00'",
+        "$limit": 10
+    }
+
+    assert actual == expected
